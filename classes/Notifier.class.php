2f70622a0964d16873d72c9bf8e7d57e2294965e
Lee Garner <lee@leegarner.com>
2022-06-15 11:14:36 -0700

    Uikit3 form spec

--- classes/Notifier.class.php
+++ classes/Notifier.class.php
@@ -1,36 +1,40 @@
 <?php
 /**
- * Class to send notifications to subscribers.
+ * This class provides two functions.
+ * 1. Send confirmations to subscribers via the glFusion Email notifier.
+ * 2. Queue messages to be sent on behalf of this or other plugins.
  *
  * @author      Lee Garner <lee@leegarner.com>
- * @copyright   Copyright (c) 2020 Lee Garner <lee@leegarner.com>
+ * @copyright   Copyright (c) 2020-2022 Lee Garner <lee@leegarner.com>
  * @package     mailer
- * @version     v0.0.4
+ * @version     v0.2.0
  * @since       v0.0.4
  * @license     http://opensource.org/licenses/gpl-2.0.php
  *              GNU Public License v2 or later
  * @filesource
  */
 namespace Mailer;
+use Mailer\Models\Campaign;
+use Mailer\Models\Queue;
 
 
 /**
- * Notification class to send opt-in emails to subscribers.
+ * Notification class to send emails to subscribers.
  * @package shop
  */
-class Notifier
+class Notifier extends \glFusion\Notifiers\Email
 {
     /**
-     * Send an email to the administrator and/or buyer.
+     * Send an opt-in confirmation to the subscriber
      *
-     * @param   string  $status     Order status (pending, paid, etc.)
-     * @param   string  $gw_msg     Optional gateway message to include with email
+     * @param   string  $email  Subscriber's email address
+     * @param   string  $token  Token included to validate unsubscribe requests.
      */
-    public static function Send($email, $token)
+    public static function sendConfirmation($email, $token)
     {
         global $_CONF, $LANG_MLR;
 
-        $title = $_CONF['site_name'] . ' ' . $LANG_MLR['confirm_title'];
+        //$title = $_CONF['site_name'] . ' ' . $LANG_MLR['confirm_title'];
 
         // TODO - use a template for this
         $templatepath = Config::get('pi_path') . '/templates/';
@@ -48,12 +52,44 @@ class Notifier
             'confirm_period' => Config::get('confirm_period'),
             'site_name'     => $_CONF['site_name'],
         ) );
-
         $T->parse('output', 'message');
         $body = $T->finish($T->get_var('output'));
-        $altbody = strip_tags($body);
-        $from = array(Config::senderEmail(), Config::senderName());
-        COM_mail($email, $title, $body, $from, true, 0, '', $altbody);
+
+        $Emailer = \glFusion\Notifier::getProvider('Email');
+        $Emailer->setMessage($body, true)
+              ->setSubject($_CONF['site_name'] . ' ' . $LANG_MLR['confirm_title'])
+              ->addRecipient(0, '', $email)
+              ->setFromEmail(Config::senderEmail())
+              ->setFromName(Config::senderName())
+              ->send();
+    }
+
+
+    /**
+     * Send an email notification.
+     * This function simply queues the mail, which will then be sent by
+     * Queue::process().
+     *
+     * @return  boolean     True on success, False on error
+     */
+    public function send() : bool
+    {
+        $status = false;
+
+        // Create the mailing and recipient list.
+        $Mlr = new Campaign;
+        $status = $Mlr->withContent($this->htmlmessage)
+            ->withTitle($this->subject)
+            ->withTemplate(false)
+            ->withProvider('Internal')
+            ->withGroup(0)      // no user access to the mailer
+            ->withExpDays(5)    // probably don't need to keep even this long
+            ->Save();
+        if ($status) {
+            Queue::addEmails($Mlr->getID(), $this->prepareRecipients($this->recipients));
+            Queue::addEmails($Mlr->getID(), $this->prepareRecipients($this->bcc));
+        }
+        return $status;
     }
 
 }
